d1ece6ffb5fc8141938d66fc5513486dc09cd4e1
khemraj-1103 <khemraj@bitcot.com>
2021-08-03 17:57:24 +0530

    New JavaScript code written

--- index.php
+++ index.php
@@ -6,3 +6,13 @@ echo 'this is patiyala house';
 echo 'hi this is nilesh';
 echo 'Wordpress setup need to updtae';
 ?>
+
+<script type="text/javascript">
+	
+	(function( $ ) {
+
+		alert('Hello Wordress');
+
+	})(jQuery);
+
+</script>
